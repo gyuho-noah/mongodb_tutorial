74d3834601af7443f0bf1ba9a8fae376cb61455c
이규호 <gyuho@igyuhoui-Macmini.local>
2025-06-24 10:56:05 +0900

    third

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const { generateFake2Data } = require('../faker2');
 
 const server = async() => {
     try {
-        const { MONGO_URI } = process.env;
+        const { MONGO_URI, PORT } = process.env;
         if(!MONGO_URI) throw new Error("MONGO_URI is required!!");
 
         await mongoose.connect(MONGO_URI);
@@ -17,8 +17,8 @@ const server = async() => {
         app.use(express.json());
         app.use('/user', userRouter);
         app.use('/blog', blogRouter);
-        app.listen(3000, async () => {
-            console.log('server listen');
+        app.listen(PORT, async () => {
+            console.log(`server listening on p[ort ${PORT}`);
             // await generateFakeData(1000000, 5, 20);
             // for(let i=0; i<20; i++) {
             //     await generateFake2Data(10, 2, 10);
